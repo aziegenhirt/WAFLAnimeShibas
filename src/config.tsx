3f6e91e2346ad66e6d03724d12784d34cd5a6d2f
nipud0s <nipudos-01@yahoo.com>
2022-01-12 16:22:18 +0000

    Shibas SC address

--- src/config.tsx
+++ src/config.tsx
@@ -13,7 +13,7 @@ export const walletConnectDeepLink =
   "https://maiar.page.link/?apn=com.elrond.maiar.wallet&isi=1519405832&ibi=com.elrond.maiar.wallet.dev&link=https://maiar.com/";
 
 export const contractAddress =
-  "erd1qqqqqqqqqqqqqpgqlcj7fu5qkygn7l96h407p3efypzcp8k3y3ys6vtdan";
+  "erd1qqqqqqqqqqqqqpgqwqdtqlqep7cyqx7thd8cntz20neen6hry3ys9nx43u";
 
 export const network: Dapp.NetworkType = {
   id: "devnet",
